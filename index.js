9c1977f3cb8c1bf0e454d59c177e6a53319de98c
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-23 12:39:51 -0700

    Bundle spdx- packages

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-var parser = require('./parser.generated.js').parser;
-
-exports.parse = function(argument) {
-  return parser.parse(argument);
-};
-
-var containsRepeatedSpace = /\s{2,}/;
-
-exports.valid = function(argument) {
-  if (
-    argument.trim() !== argument ||
-    containsRepeatedSpace.test(argument)
-  ) {
-    return false;
-  }
-  try {
-    parser.parse(argument);
-    return true;
-  } catch (e) {
-    // jison generates parsers that throw errors, while this function
-    // mimics `semver.valid` by returning null.
-    return null;
-  }
-};
-
-// Require the same license and exception data used by the parser.
-exports.licenses = require('spdx-license-ids');
-exports.exceptions = require('spdx-exceptions');
-
-// The License Expression Syntax version
-exports.specificationVersion = '2.0';
+exports.compare = require('spdx-compare')
+exports.parse = require('spdx-expression-parse')
+exports.validate = require('spdx-expression-validate')
